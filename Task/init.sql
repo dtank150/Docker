4cb9fc97e1568a0e8db165bf41389fb505d8b9bb
dtank150 <dtank150@rku.ac.in>
2024-02-25 22:15:54 +0530

    Docker

---
+++ Task/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS project_db;
+USE project_db;
+
+CREATE USER 'project_user'@'%' IDENTIFIED BY 'project_password';
+CREATE DATABASE IF NOT EXISTS project_db;
+GRANT ALL PRIVILEGES ON project_db.* TO 'project_user'@'%';
